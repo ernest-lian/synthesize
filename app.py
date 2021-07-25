7206c388e17c880e15477ab9df0a8f28cc1a52ec
Ernest Lian <ernest.lian97@hotmail.com>
2021-07-25 00:09:56 -0400

    Add port configuration

--- app.py
+++ app.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+	# Bind to PORT if defined, otherwise default to 5000.
+	port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
