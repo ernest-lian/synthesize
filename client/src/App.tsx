cd5856f3664894406a22e6dada49fb04e8e5d438
Ernest Lian <ernest.lian97@hotmail.com>
2021-08-02 14:28:07 -0400

    Refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
+
 import './App.css'; 
+import Loading from './user_interface/loading.tsx'
 
 function api(){
   return fetch("http://localhost:5000/testing", {
@@ -43,20 +44,7 @@ const App = ({
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Loading/>
       <p>{text}</p>
       <button onClick={() => handleClick()}></button>;
     </div>
--- client/src/user_interface/loading.tsx
+++ client/src/user_interface/loading.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { Button } from '@material-ui/core';
 
 const Loading = ({
     
 }) => {
-  
+  return(
+    <Button color="primary"> Hello world </Button>
+  )
 }
 
 export default Loading;
