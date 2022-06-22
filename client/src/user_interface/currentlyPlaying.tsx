5c4517de2385865d55646f9a55f7c149b2a1674b
Ernest Lian <ernest.lian97@hotmail.com>
2022-06-21 22:19:59 -0400

    Refactor

--- client/src/user_interface/currentlyPlaying.tsx
+++ client/src/user_interface/currentlyPlaying.tsx
@@ -1,10 +1,17 @@
+import  { useState } from 'react';
 import { Box, Typography } from '@material-ui/core';
 import PauseIcon from '@mui/icons-material/Pause';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 import '../styling/currentlyPlaying.css';
 
 
 const CurrentlyPlaying = () => {
+  const [playing, setPlaying] = useState(false);
+
+  const handleSetPlaying = () => setPlaying(!playing);
+
+
   return(
     <Box
       id='currently-playing'
@@ -21,11 +28,11 @@ const CurrentlyPlaying = () => {
           flexDirection='column'
           textAlign='left'
         >
-          <Typography >Title</Typography>
-          <Typography >Artist</Typography>
+          <Typography id='title'>Title</Typography>
+          <Typography id='artist'>Artist</Typography>
         </Box>
       </Box>
-      <PauseIcon style={{ 'alignSelf': 'center'}}/>
+      {playing ? <PlayArrowIcon onClick = {() => handleSetPlaying} style={{ color: '#1DB954'}}/> : <PauseIcon onClick = {() => handleSetPlaying} style={{ 'alignSelf': 'center', color: '#1DB954'}}/>}
     </Box>
     )
 }
--- client/src/user_interface/remix.tsx
+++ client/src/user_interface/remix.tsx
@@ -28,6 +28,36 @@ const Remix = () => {
         <RemixComponent 
           number={3} cover={Ctrl} title="STUD" artist="Troye Sivan"
         />
+        <RemixComponent 
+          number={4} cover={Ctrl} title="STUD" artist="Troye Sivan"
+        />
+        <RemixComponent 
+          number={5} cover={Ctrl} title="STUD" artist="Troye Sivan"
+        />
+        <RemixComponent 
+          number={6} cover={Ctrl} title="STUD" artist="Troye Sivan"
+        />
+        <RemixComponent 
+          number={7} cover={Ctrl} title="STUD" artist="Troye Sivan"
+        />
+        <RemixComponent 
+          number={8} cover={Ctrl} title="STUD" artist="Troye Sivan"
+        />
+        <RemixComponent 
+          number={9} cover={Ctrl} title="STUD" artist="Troye Sivan"
+        />
+        <RemixComponent 
+          number={10} cover={Ctrl} title="STUD" artist="Troye Sivan"
+        />
+        <RemixComponent 
+          number={11} cover={Ctrl} title="STUD" artist="Troye Sivan"
+        />
+        <RemixComponent 
+          number={12} cover={Ctrl} title="STUD" artist="Troye Sivan"
+        />
+        <RemixComponent 
+          number={13} cover={Ctrl} title="STUD" artist="Troye Sivan"
+        />
       </Box>
     </Box>
     )
